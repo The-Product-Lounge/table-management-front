2f59b099446df4258cfe9c676da08dec953cb5b2
YasminGd <thegodha1@gmail.com>
2023-02-24 01:17:31 +0200

    added a You to user preview

--- src/cmps/UserPreview.jsx
+++ src/cmps/UserPreview.jsx
@@ -1,50 +1,55 @@
-import { useLocation } from 'react-router-dom'
-import tableMemberMenu from '../assets/imgs/table-member-menu.svg'
-import { Tooltip } from 'react-tooltip'
-import 'react-tooltip/dist/react-tooltip.css'
-import removeLounger from '../assets/imgs/remove-member.svg'
+import { useLocation } from "react-router-dom"
+import tableMemberMenu from "../assets/imgs/table-member-menu.svg"
+import { Tooltip } from "react-tooltip"
+import "react-tooltip/dist/react-tooltip.css"
+import removeLounger from "../assets/imgs/remove-member.svg"
+import { useSelector } from "react-redux"
 
 export const UserPreview = ({ user, onRemoveLounger }) => {
   const location = useLocation()
-  const isLocationTableView = location.pathname.includes('/table')
+  const isLocationTableView = location.pathname.includes("/table")
+  const userFromState = useSelector((state) => state.userModule.user)
 
   return (
-    <section className="user-preview">
-      <div className="profile-img-container">
-        <img src={user.imgUrl} alt="" className="profile-img" loading="lazy" />
+    <section className='user-preview'>
+      <div className='profile-img-container'>
+        <img src={user.imgUrl} alt='' className='profile-img' loading='lazy' />
       </div>
-      <div className="user-details">
-        <p>
-          <span className="first-name">{user.firstName}</span> {user.lastName}
-        </p>
+      <div className='user-details'>
+        <div className='user-name-and-you'>
+          <p>
+            <span className='first-name'>{user.firstName}</span> {user.lastName}
+          </p>
+          {userFromState.id === user.id && <p className="you">You</p>}
+        </div>
         {!isLocationTableView && (
           <img
             id={`tooltip-${user.id}`}
-            className="member-menu"
+            className='member-menu'
             src={tableMemberMenu}
-            alt="Table member menu"
+            alt='Table member menu'
           />
         )}
         <Tooltip
           anchorId={`tooltip-${user.id}`}
-          place="left"
-          events={['click']}
+          place='left'
+          events={["click"]}
           style={{
-            backgroundColor: 'white',
-            color: '#28293F',
-            boxShadow: '0px 4px 12px #60617029',
-            opacity: '1',
-            pointerEvents: 'auto',
+            backgroundColor: "white",
+            color: "#28293F",
+            boxShadow: "0px 4px 12px #60617029",
+            opacity: "1",
+            pointerEvents: "auto",
           }}
         >
           <div
-            className="tooltip-container"
+            className='tooltip-container'
             onClick={() => onRemoveLounger(user.id)}
           >
             <img
-              className="remove-lounger"
+              className='remove-lounger'
               src={removeLounger}
-              alt="remove lounger"
+              alt='remove lounger'
             />
             <span>Remove Lounger</span>
           </div>
