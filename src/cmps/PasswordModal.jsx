1c5ec1180cdd508a5ed696b0b361b6d3d591b5b6
ohayonb <ohayonzzb@gmail.com>
2023-02-26 14:07:47 +0200

    form padding top & bottom changed. placeholder selection removed. loader added to EventSettings

--- src/cmps/PasswordModal.jsx
+++ src/cmps/PasswordModal.jsx
@@ -27,7 +27,7 @@ export const PasswordModal = ({ onToggleModal, classes }) => {
 
   const onCheckPassword = (event) => {
     event.preventDefault()
-    if (passwordInput === 'TPL!1995') {
+    if (passwordInput === process.env.REACT_APP_ADMIN_PASSWORD) {
       navigate('/event-settings')
     } else {
       setWrongPassword(true)
