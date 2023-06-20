f1fc2555203ac877711c56cd979b96d51c5e7975
AlmogH <almoghagbi96@gmail.com>
2023-06-20 13:44:08 +0300

    Adding theme and login route

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import { Form } from "./views/Form";
 import { TableView } from "./views/TableView";
 import { EventSettings } from "./views/EventSettings";
+import { Login } from "./views/Login";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "./theme/theme";
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Routes>
         <Route path="" element={<Form />} />
         <Route path="event-settings" element={<EventSettings />} />
         <Route path="table/:tableId" element={<TableView />} />
+        <Route path="login" element={<Login />} />
       </Routes>
-    </div>
+    </ThemeProvider>
   );
 }
 
