6feee989e8ba89a8ddb1c4c154969e56d07c6aa4
AlmogH <almoghagbi96@gmail.com>
2023-06-21 17:28:08 +0300

    add text helper style

--- src/cmps/inputs/TextField.jsx
+++ src/cmps/inputs/TextField.jsx
@@ -1,9 +1,26 @@
-import { IconButton, InputAdornment } from "@mui/material";
+import { IconButton, InputAdornment, Stack } from "@mui/material";
 import MuiTextField from "@mui/material/TextField";
 import React, { useState } from "react";
 
 import openEye from "../../assets/imgs/open-eye.svg";
 import closedEye from "../../assets/imgs/closed-eye.svg";
+import redError from "../../assets/imgs/red-error.svg";
+
+const HelperTextError = ({ children }) => {
+  return (
+    <Stack direction="row" alignItems="center">
+      <img
+        src={redError}
+        alt="red-error"
+        style={{
+          width: 16,
+          height: 16,
+        }}
+      />
+      {children}
+    </Stack>
+  );
+};
 
 /**
  * TextField - A component that renders a text field from material ui,
@@ -12,14 +29,21 @@ import closedEye from "../../assets/imgs/closed-eye.svg";
 export const TextField = React.forwardRef((props, ref) => {
   const [showPassword, setShowPassword] = useState(false);
 
+  let reProps = { ...props };
+  const helperText = reProps.helperText;
+
+  if (helperText && props.error) {
+    reProps.helperText = <HelperTextError>{helperText}</HelperTextError>;
+  }
+
   return (
     <>
       {props.type !== "password" ? (
-        <MuiTextField ref={ref} {...props} />
+        <MuiTextField ref={ref} {...reProps} />
       ) : (
         <MuiTextField
           ref={ref}
-          {...props}
+          {...reProps}
           type={showPassword ? "text" : "password"}
           InputProps={{
             endAdornment: (
