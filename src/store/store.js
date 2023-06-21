2daf932d6724aa8afb41a02296bc00e52ea9b465
AlmogH <almoghagbi96@gmail.com>
2023-06-21 17:27:22 +0300

    adding auth module to redux store

--- src/store/store.js
+++ src/store/store.js
@@ -6,9 +6,11 @@ import {
 } from "redux";
 import thunk from "redux-thunk";
 import { userReducer } from "./reducers/user.reducer";
+import { authReducer } from "./reducers/auth.reducer";
 
 const rootReducer = combineReducers({
   userModule: userReducer,
+  authModule: authReducer,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
