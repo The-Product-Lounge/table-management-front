27eb20cc9305ecdc2aee47870ebe3defc331c047
AlmogH <almoghagbi96@gmail.com>
2023-06-21 17:28:41 +0300

    adding private routes

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ import { EventSettings } from "./views/EventSettings";
 import { Login } from "./views/Login";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./theme/theme";
+import { useSelector } from "react-redux";
+import { ProtectedRoute } from "./views/PrivateRoutes";
 
 function App() {
+  const jwt_token = useSelector((state) => state.authModule.jwt_token);
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Routes>
         <Route path="" element={<Form />} />
-        <Route path="event-settings" element={<EventSettings />} />
+        <Route element={<ProtectedRoute isAllowed={!!jwt_token} />}>
+          <Route path="event-settings" element={<EventSettings />} />
+        </Route>
         <Route path="table/:tableId" element={<TableView />} />
         <Route path="login" element={<Login />} />
       </Routes>
