72d8b22d8b15280130471eb87eed102b25e2f6c5
AlmogH <almoghagbi96@gmail.com>
2023-07-11 16:35:30 +0300

    Start fresh

--- src/views/PrivateRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-export const ProtectedRoute = ({ isAllowed, redirectPath = "/", children }) => {
-  if (!isAllowed) {
-    return <Navigate to={redirectPath} replace />;
-  }
-
-  return children ? children : <Outlet />;
-};
