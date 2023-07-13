9638eb4f52b5ae2b9f1cc8403b1923e472f45b77
AlmogH <almoghagbi96@gmail.com>
2023-07-13 15:10:11 +0300

    edit firebase todo

--- src/old/firebase-setup/firebase.js
+++ src/old/firebase-setup/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
+// TODO: fix import on server side problem because of window
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
