02dbc94469620f07712bac14de770e4c25ada820
AlmogH <almoghagbi96@gmail.com>
2023-07-18 11:36:31 +0300

    Remove old events.js file and remove unused hook

--- src/old/views/EventsView.jsx
+++ src/old/views/EventsView.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { AppHeader } from "../components/AppHeader";
 import { EventList } from "../components/events-view/EventList";
 import { EmptyEventList } from "../components/events-view/EmptyEventList";
-import { events as eventsFromDB } from "../events.js";
 import { off, onValue, ref } from "firebase/database";
 import { database } from "../firebase-setup/firebase";
 import { utilService } from "../services/util.service";
@@ -10,7 +9,6 @@ import { utilService } from "../services/util.service";
 export const EventsView = () => {
   const [events, setEvents] = useState([]);
   const [tables, setTables] = useState([]);
-  const [isFirstHookCompleted, setIsFirstHookCompleted] = useState(false);
 
   //! doing this like this allows for UI rerendering when tables change but that means
   //that there cant be two tables with the same table number in different events
@@ -21,26 +19,23 @@ export const EventsView = () => {
       const data = snapshot.val();
       const events = utilService.reformatKeyValuePairToArray(data);
       setEvents(events);
-      setIsFirstHookCompleted(true);
     });
 
     return () => off(eventsRef, "value", listener);
   }, []);
 
   useEffect(() => {
-    if (isFirstHookCompleted) {
-      const tablesRef = ref(database, `/tables`);
-      const listener = onValue(tablesRef, (snapshot) => {
-        const data = snapshot.val();
-        const tables = utilService.reformatKeyValuePairToArray(
-          data,
-          "tableNumber"
-        );
-        setTables(tables);
-      });
+    const tablesRef = ref(database, `/tables`);
+    const listener = onValue(tablesRef, (snapshot) => {
+      const data = snapshot.val();
+      const tables = utilService.reformatKeyValuePairToArray(
+        data,
+        "tableNumber"
+      );
+      setTables(tables);
+    });
 
-      return () => off(tablesRef, "value", listener);
-    }
+    return () => off(tablesRef, "value", listener);
   }, [events]);
 
   return (
