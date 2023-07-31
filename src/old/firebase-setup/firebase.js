e0f49da4971bcfd9fd23ef33e443a6fa7fcdfa06
AlmogH <almoghagbi96@gmail.com>
2023-07-31 15:56:20 +0300

    change firebase config to be from environment variables

--- src/old/firebase-setup/firebase.js
+++ src/old/firebase-setup/firebase.js
@@ -8,15 +8,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBpBDwmI88LI3mhdhW77DUNIl9ZmsFlUu8",
-  authDomain: "table-management-lounge.firebaseapp.com",
-  databaseURL:
-    "https://table-management-lounge-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "table-management-lounge",
-  storageBucket: "table-management-lounge.appspot.com",
-  messagingSenderId: "284378515607",
-  appId: "1:284378515607:web:f530de6965c37d355401d2",
-  measurementId: "G-JS3L2FZKJ6",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
