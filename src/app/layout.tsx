675c9323951f2625c2598d945a8e50a78c19f9aa
AlmogH <almoghagbi96@gmail.com>
2023-07-31 18:32:09 +0300

    use viewport meta to fix auto zoom in iphone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "TPL | MySeat",
   description: "Table management platform by The Product Lounge",
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+  },
 };
 
 export default function RootLayout({
